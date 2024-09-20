2ee538b74feb37ade1aee56c28b71ffae3816697
dispear <kkm7hjh@naver.com>
2024-09-20 14:29:44 +0900

    123

---
+++ src/test/java/com/pettory/pettory/PettoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pettory.pettory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
