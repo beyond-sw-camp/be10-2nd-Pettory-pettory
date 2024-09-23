a74bcf656789b4c0ee5b186f338de49fe7fef904
aosskfdlrla <gmltns885@gmail.com>
2024-09-23 17:20:43 +0900

    #1 [FEAT] 테스트코드 작성 및 html 추가 - 수정 : PettoryChatController - 추가 : testChatting.html

--- src/main/java/com/pettory/pettory/chat/controller/PettoryChatController.java
+++ src/main/java/com/pettory/pettory/chat/controller/PettoryChatController.java
@@ -1,8 +1,14 @@
 package com.pettory.pettory.chat.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PettoryChatController {
 
+    /* Test 를 위한 testChatting */
+    @GetMapping("/testChatting")
+    public String testChattingFeature() {
+        return "testChatting";
+    }
 }
