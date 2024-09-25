c9f224c3498833525ef347caf60cf2d1d16c78e2
aosskfdlrla <gmltns885@gmail.com>
2024-09-25 14:31:56 +0900

    #1 [FEAT] ChatBeanConfig 추가 , Service 수정 - ModelMapper 사용하기 위함

---
+++ src/main/java/com/pettory/pettory/chat/config/ChatBeanConfig.java
@@ -0,0 +1,14 @@
+package com.pettory.pettory.chat.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/* 2024.09.25 황희순 chatBeanConfig 생성 - ModelMapper 를 사용하기 위함. */
+@Configuration
+public class ChatBeanConfig {
+
+//    @Bean
+//    public ModelMapper modelmapper = new ModelMapper();
+//    /*  */
+}
--- src/main/java/com/pettory/pettory/chat/service/ChatService.java
+++ src/main/java/com/pettory/pettory/chat/service/ChatService.java
@@ -1,7 +1,32 @@
 package com.pettory.pettory.chat.service;
 
+import com.pettory.pettory.chat.repository.*;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
+    private final ModelMapper chatModelMapper;
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChattingFileRepository chattingFileRepository;
+    private final ChattingRepository chattingRepository;
+    private final VoteChooseRepository voteChooseRepository;
+    private final VoteChooseResultRepository voteChooseResultRepository;
+    private final VoteRepository voteRepository;
+
+    public ChatService(ChatRoomRepository chatRoomRepository,
+                       ChattingFileRepository chattingFileRepository,
+                       ChattingRepository chattingRepository,
+                       VoteChooseRepository voteChooseRepository,
+                       VoteChooseResultRepository voteChooseResultRepository,
+                       VoteRepository voteRepository,
+                       ModelMapper chatModelMapper) {
+        this.chatModelMapper = chatModelMapper;
+        this.chatRoomRepository = chatRoomRepository;
+        this.chattingFileRepository = chattingFileRepository;
+        this.chattingRepository = chattingRepository;
+        this.voteChooseRepository = voteChooseRepository;
+        this.voteChooseResultRepository = voteChooseResultRepository;
+        this.voteRepository = voteRepository;
+    }
 }
