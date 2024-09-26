039fe627cb1656c95c1288d02bb632e06b6be62f
kimjm9911 <kimjm9911@naver.com>
2024-09-26 17:29:16 +0900

    파일이름수정

--- src/main/java/com/pettory/pettory/walkinggroup/command/infrastructure/repository/JpaWalkingGroupRepository.java
+++ src/main/java/com/pettory/pettory/walkinggroup/command/infrastructure/repository/JpaWalkingGroupRepository.java
@@ -4,5 +4,5 @@ import com.pettory.pettory.walkinggroup.command.domain.aggregate.WalkingGroup;
 import com.pettory.pettory.walkinggroup.command.domain.repository.WalkingGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaProductRepository extends WalkingGroupRepository, JpaRepository<WalkingGroup, Integer> {
+public interface JpaWalkingGroupRepository extends WalkingGroupRepository, JpaRepository<WalkingGroup, Integer> {
 }
