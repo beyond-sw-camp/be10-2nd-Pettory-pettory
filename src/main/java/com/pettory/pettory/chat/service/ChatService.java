e88db41f2b694c22021f05b3749c6a7d2ffac7e2
aosskfdlrla <gmltns885@gmail.com>
2024-09-28 11:17:34 +0900

    #1 [FEAT] 채팅 수정 삭제 기능 구현

--- src/main/java/com/pettory/pettory/chat/service/ChatService.java
+++ src/main/java/com/pettory/pettory/chat/service/ChatService.java
@@ -1,16 +1,15 @@
 package com.pettory.pettory.chat.service;
 
 import com.pettory.pettory.chat.dto.ChatRoomDTO;
-import com.pettory.pettory.chat.dto.ChattingDTO;
+import com.pettory.pettory.chat.dto.chatting.InsertChattingDTO;
+import com.pettory.pettory.chat.dto.chatting.ModifyChattingDTO;
+import com.pettory.pettory.chat.dto.chatting.SoftDeleteChattingDTO;
 import com.pettory.pettory.chat.entity.ChatRoom;
 import com.pettory.pettory.chat.entity.Chatting;
-import com.pettory.pettory.chat.enums.ChatRoomStateEnum;
 import com.pettory.pettory.chat.repository.*;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.awt.*;
 
 @Service
 public class ChatService {
@@ -40,8 +39,8 @@ public class ChatService {
 
     /* 1. 채팅방 추가 */
     @Transactional
-    public void registerChatRoom(ChatRoomDTO chatRoomDTO) {
-        chatRoomRepository.save(chatModelMapper.map(chatRoomDTO, ChatRoom.class));
+    public ChatRoom registerChatRoom(ChatRoomDTO chatRoomDTO) {
+        return chatRoomRepository.save(chatModelMapper.map(chatRoomDTO, ChatRoom.class));
     }
 
     /* 2. 채팅방 상태 수정 */
@@ -53,7 +52,59 @@ public class ChatService {
         chatRoom.modifyChatRoomDeleteTime(chatRoomDTO.getChatRoomDeleteTime());
     }
 
-    public void registerChatting(ChattingDTO chattingDTO) {
-        chattingRepository.save(chatModelMapper.map(chattingDTO, Chatting.class));
+    /* 3. 채팅 내용 DB 저장 */
+    @Transactional
+    public void registerChatting(InsertChattingDTO insertChattingDTO) {
+        chattingRepository.save(chatModelMapper.map(insertChattingDTO, Chatting.class));
+    }
+
+    /* 4. 채팅 내용 수정 */
+    @Transactional
+    public void modifyChatting(ModifyChattingDTO modifyChattingDTO) {
+        Chatting chatting = chattingRepository.findById(modifyChattingDTO.getChattingUniqueNum()).orElseThrow(IllegalArgumentException::new);
+        /* 채팅 수정 메소드 */
+        chatting.modifyChatting(modifyChattingDTO.getChattingContent(),
+                                modifyChattingDTO.getChattingUpdateTime(),
+                                modifyChattingDTO.getChattingState());
+    }
+
+    /* 5-1. 채팅 얇은 삭제 */
+    @Transactional
+    public void softDeleteChatting(SoftDeleteChattingDTO softDeleteChattingDTO) {
+        Chatting chatting = chattingRepository.findById(softDeleteChattingDTO.getChattingUniqueNum()).orElseThrow(IllegalArgumentException::new);
+        /* 채팅 얇은 삭제 기능 메소드 : 상태 변경, deleteTime 삽입 */
+        chatting.softDeleteChatting(softDeleteChattingDTO.getChattingState(),
+                                    softDeleteChattingDTO.getChattingDeleteTime());
+    }
+
+    /* 5-2. 채팅 리얼 삭제 */
+    @Transactional
+    public void hardDeleteChatting(Integer chattingUniqueNum) {
+        chattingRepository.deleteById(chattingUniqueNum);
+    }
+
+    /* 6. 채팅 이미지 업로드 */
+    public void registerImageUpload() {
+
+    }
+
+    /* 7. 채팅 동영상 업로드 */
+    public void registerVideoUpload() {
+
+    }
+
+    /* 8. 투표 등록 */
+    public void registerVote() {
+
+    }
+
+    /* 9. 투표 수정 */
+    public void modifyVote() {
+
+    }
+
+    /* 10. 투표 삭제 */
+    public void deleteVote() {
+
     }
 }
