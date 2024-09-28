49bf51ffda06adc47dc5e1c7fe58655b0077f810
aosskfdlrla <gmltns885@gmail.com>
2024-09-28 11:16:25 +0900

    #1 [FEAT] Chatting Entity 메소드 추가

--- src/main/java/com/pettory/pettory/chat/entity/Chatting.java
+++ src/main/java/com/pettory/pettory/chat/entity/Chatting.java
@@ -24,4 +24,24 @@ public class Chatting {
     private LocalDateTime chattingDeleteTime;
     private String chattingState;
     private int userId;
+
+    /* 채팅 수정 기능
+    * 1) chattingContent : 수정할 채팅 내용
+    * 2) updateTime : 채팅 수정 일시
+    * 3) chattingState : 채팅 상태
+    * */
+    public void modifyChatting(String chattingContent, LocalDateTime updateTime, String chattingState) {
+      this.chattingContent = chattingContent;
+      this.chattingUpdateTime = updateTime;
+      this.chattingState = chattingState;
+    }
+
+    /* 채팅 얇은 삭제 기능
+    * 1) chattingState : 채팅 상태
+    * 2) deleteTime : 삭제 시간
+    * */
+    public void softDeleteChatting(String chattingState, LocalDateTime deleteTime) {
+        this.chattingState = chattingState;
+        this.chattingDeleteTime = deleteTime;
+    }
 }
