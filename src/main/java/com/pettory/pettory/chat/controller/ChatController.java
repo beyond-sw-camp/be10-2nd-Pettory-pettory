49b1630d8b175d760196c976a8fa67dfc08cfaf4
aosskfdlrla <gmltns885@gmail.com>
2024-09-30 11:47:07 +0900

    #1 [FEAT] 채팅 조회 Controller 구현 - selectChatRoomChatting 메소드 추가 - ResponseEntity 응답 설정

--- src/main/java/com/pettory/pettory/chat/controller/ChatController.java
+++ src/main/java/com/pettory/pettory/chat/controller/ChatController.java
@@ -2,15 +2,23 @@ package com.pettory.pettory.chat.controller;
 
 import com.pettory.pettory.chat.dto.chatting.InsertChattingDTO;
 import com.pettory.pettory.chat.dto.chatting.ModifyChattingDTO;
+import com.pettory.pettory.chat.dto.chatting.SelectChattingDTO;
 import com.pettory.pettory.chat.dto.chatting.SoftDeleteChattingDTO;
 import com.pettory.pettory.chat.entity.ChatRoom;
 import com.pettory.pettory.chat.dto.ChatRoomDTO;
+import com.pettory.pettory.chat.response.ResponseSelectChattingMessage;
 import com.pettory.pettory.chat.service.ChatService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/chat")
@@ -56,6 +64,23 @@ public class ChatController {
         return ResponseEntity.created(
                 URI.create("/chat/chatroom-state/" + chatRoomDTO.getChatRoomUniqueNum())
         ).build();
+    }
+
+    /* 채팅방의 채팅 내용 조회 */
+    @GetMapping("/chatroom-chatting/{chatRoomUniqueNum}")
+    public ResponseEntity<ResponseSelectChattingMessage> selectChatRoomChatting(@PathVariable Integer chatRoomUniqueNum) {
+        /* 응답헤더 설정 */
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+
+        List<SelectChattingDTO> chattingList = chatService.selectChatRoomChatting(chatRoomUniqueNum);
+
+        /* 응답 데이터 설정 */
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("chattingList", chattingList);
+        ResponseSelectChattingMessage responseMessage = new ResponseSelectChattingMessage(200, "조회 성공", responseMap);
+
+        return ResponseEntity.ok().headers(headers).body(responseMessage);
     }
 
     /* 3. 채팅방의 채팅 내용을 저장 */
