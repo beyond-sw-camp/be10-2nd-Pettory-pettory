2833ff63110f5795dfc692f8604585215f0966ef
aosskfdlrla <gmltns885@gmail.com>
2024-09-30 15:37:05 +0900

    #1 [FEAT] Mapper 추가 - VoteMapper.java - VoteMapper.xml

--- src/main/java/com/pettory/pettory/chat/mapper/VoteMapper.java
+++ src/main/java/com/pettory/pettory/chat/mapper/VoteMapper.java
@@ -1,4 +1,9 @@
 package com.pettory.pettory.chat.mapper;
 
+import com.pettory.pettory.chat.dto.vote.SelectVoteDTO;
+
+import java.util.List;
+
 public interface VoteMapper {
+    List<SelectVoteDTO> selectVoteList(Integer voteChatroomUniqueNum);
 }
