637e0a39e0853f3da3c3b5161a207ce294f70652
kimjm9911 <kimjm9911@naver.com>
2024-10-02 08:46:50 +0900

    충돌수정

--- src/main/java/com/pettory/pettory/config/JpaConfig.java
+++ src/main/java/com/pettory/pettory/config/JpaConfig.java
@@ -4,6 +4,5 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
 public class JpaConfig {
 }
