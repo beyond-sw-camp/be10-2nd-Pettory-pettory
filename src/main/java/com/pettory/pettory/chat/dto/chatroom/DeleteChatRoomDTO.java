babfdf0cea1ed0119abba691b2a4db4d9d009b0b
aosskfdlrla <gmltns885@gmail.com>
2024-10-02 16:16:18 +0900

    #30 [DOCS] Swagger 적용 - ChatController - DeleteChatRoomDTO

--- src/main/java/com/pettory/pettory/chat/dto/chatroom/DeleteChatRoomDTO.java
+++ src/main/java/com/pettory/pettory/chat/dto/chatroom/DeleteChatRoomDTO.java
@@ -1,5 +1,6 @@
 package com.pettory.pettory.chat.dto.chatroom;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,10 +12,18 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 @AllArgsConstructor
+@Schema(description = "삭제할 채팅방 DTO 정보")
 public class DeleteChatRoomDTO {
+    @Schema(description = "채팅방 고유번호")
     private int chatroomUniqueNum;
+
+    @Schema(description = "채팅방 업데이트 일시")
     private LocalDateTime chatroomUpdateTime;
+
+    @Schema(description = "채팅방 삭제 일시")
     private LocalDateTime chatroomDeleteTime;
+
+    @Schema(description = "채팅방 상태")
     private String chatroomState;
 
     public DeleteChatRoomDTO(int chatroomUniqueNum) {
