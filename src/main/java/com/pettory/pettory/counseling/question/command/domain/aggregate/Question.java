18bce4c21b3f98997997f7e0053aed6fa986c3bd
ygc1994 <ygc1994@naver.com>
2024-10-04 06:37:38 +0900

    #55 [FEAT] 질문 엔터티 수정 -Question

--- src/main/java/com/pettory/pettory/counseling/question/command/domain/aggregate/Question.java
+++ src/main/java/com/pettory/pettory/counseling/question/command/domain/aggregate/Question.java
@@ -13,22 +13,33 @@ public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "counseling_question_num")
     private int counselingQuestionNum;
+
+    @Column(name = "user_id")
     private int userId;
+
+    @Column(name = "counseling_question_title")
     private String counselingQuestionTitle;
+
+    @Column(name = "counseling_question_content")
     private String counselingQuestionContent;
+
+    @Column(name = "counseling_question_hits")
     private int counselingQuestionHits;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "counseling_question_state")
     private QuestionState counselingQuestionState  = QuestionState.ACTIVE;
 
+    @Column(name = "counseling_question_insert_datetime")
     private String counselingQuestionInsertDatetime;
-    private String counselingQuestionDeleteDatetime;
-    private String counselingQuestionUpdateDatetime;
 
-    public void increaseCounselingQuestionHits(int counselingQuestionHits) {
-        this.counselingQuestionHits = counselingQuestionHits;
-    }
+    @Column(name = "counseling_question_delete_datetime")
+    private String counselingQuestionDeleteDatetime;
+
+    @Column(name = "counseling_question_update_datetime")
+    private String counselingQuestionUpdateDatetime;
 
     public void modifyCounselingQuestion(String counselingQuestionTitle, String counselingQuestionContent, String counselingQuestionUpdateDatetime) {
         this.counselingQuestionTitle = counselingQuestionTitle;
@@ -36,7 +47,11 @@ public class Question {
         this.counselingQuestionUpdateDatetime = counselingQuestionUpdateDatetime;
     }
 
-    public void deleteCounselingQuestion(QuestionState counselingQuestionState, String counselingQuestionDeleteDatetime) {
+    public void increaseCounselingQuestionHits(int counselingQuestionHits) {
+        this.counselingQuestionHits = counselingQuestionHits;
+    }
+
+    public void removeCounselingQuestion(QuestionState counselingQuestionState, String counselingQuestionDeleteDatetime) {
         this.counselingQuestionState = counselingQuestionState;
         this.counselingQuestionDeleteDatetime = counselingQuestionDeleteDatetime;
     }
