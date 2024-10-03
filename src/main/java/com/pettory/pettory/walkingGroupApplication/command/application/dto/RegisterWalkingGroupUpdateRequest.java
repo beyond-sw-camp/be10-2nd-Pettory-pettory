5148c6a7afe92cde306272e4fb0da19551a4ac3a
kimjm9911 <kimjm9911@naver.com>
2024-10-03 09:49:02 +0900

    test 코드 작성

--- src/main/java/com/pettory/pettory/walkingGroupApplication/command/application/dto/RegisterWalkingGroupUpdateRequest.java
+++ src/main/java/com/pettory/pettory/walkingGroupApplication/command/application/dto/RegisterWalkingGroupUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.pettory.pettory.walkingGroupApplication.command.application.dto;
 
+import com.pettory.pettory.walkingGroupApplication.command.domain.aggregate.RegisterWalkingGroupState;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -9,4 +10,8 @@ public class RegisterWalkingGroupUpdateRequest {
 
     @Schema(description = "가입한 산책 모임 상태", example = "ACTIVE")
     private String registerWalkingGroupState;
+
+    public RegisterWalkingGroupUpdateRequest(String registerWalkingGroupState) {
+        this.registerWalkingGroupState = registerWalkingGroupState;
+    }
 }
