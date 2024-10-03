6e3e44299955f11b827b6ea2a29f5396754bcbfe
ygc1994 <ygc1994@naver.com>
2024-10-04 06:36:41 +0900

    #55 [FEAT] 답변 파일 DTO 수정 -AnswerFileDTO

--- src/main/java/com/pettory/pettory/counseling/answer/command/application/dto/AnswerFileDTO.java
+++ src/main/java/com/pettory/pettory/counseling/answer/command/application/dto/AnswerFileDTO.java
@@ -1,5 +1,6 @@
 package com.pettory.pettory.counseling.answer.command.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,11 +8,19 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Schema(description = "답변파일정보 DTO")
 public class AnswerFileDTO {
 
+    @Schema(description = "답변파일번호(PK)")
     private int counselingAnswerFileNum;
+
+    @Schema(description = "답변 번호(FK)")
     private int counselingAnswerNum;
+
+    @Schema(description = "답변 파일크기")
     private int counselingAnswerFileSize;
+
+    @Schema(description = "답변 파일경로")
     private int counselingAnswerFileDirectory;
 
 }
