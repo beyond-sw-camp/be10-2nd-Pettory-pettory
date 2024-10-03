3df4b76bd85b5fff603b375c04c2dc7cf950ad31
ygc1994 <ygc1994@naver.com>
2024-10-04 06:36:17 +0900

    #55 [FEAT] 답변 DTO 수정 -AnswerDTO

--- src/main/java/com/pettory/pettory/counseling/answer/command/application/dto/AnswerDTO.java
+++ src/main/java/com/pettory/pettory/counseling/answer/command/application/dto/AnswerDTO.java
@@ -1,6 +1,7 @@
 package com.pettory.pettory.counseling.answer.command.application.dto;
 
 import com.pettory.pettory.counseling.answer.command.domain.aggregate.AnswerState;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,15 +9,31 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Schema(description = "답변정보 DTO")
 public class AnswerDTO {
 
+    @Schema(description = "답변번호(PK)")
     private int counselingAnswerNum;
+
+    @Schema(description = "질문 번호(FK)")
     private int counselingQuestionNum;
+
+    @Schema(description = "답변 내용")
     private String counselingAnswerContent;
+
+    @Schema(description = "답변 상태")
     private AnswerState counselingAnswerState;
+
+    @Schema(description = "답변 등록일")
     private String counselingAnswerInsertDatetime;
+
+    @Schema(description = "답변 삭제일")
     private String counselingAnswerDeleteDatetime;
+
+    @Schema(description = "답변 수정일")
     private String counselingAnswerUpdateDatetime;
+
+    @Schema(description = "재답변 번호")
     private int counselingAnswerReanswerNum;
 
     public AnswerDTO() {
